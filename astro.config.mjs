d9156e887bd3b7e3df68b77560af15d4ff4cde8d
Joshua Huang <jos72Him@gmail.com>
2026-01-19 02:08:58 -0600

    deploy.yml and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://activelively.github.io',
+  base : '/ActiveLively.github.io/',
   integrations: [react(), preact()],
   vite: {
     plugins: [tailwindcss()]
