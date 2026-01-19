cf12038748eb094a4150043dd253b6974569a7cc
Joshua Huang <jos72Him@gmail.com>
2026-01-18 23:01:00 -0600

    tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import preact from '@astrojs/preact';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://activelively.github.io',
-  integrations: [react()],
+  integrations: [react(), preact()],
   vite: {
     plugins: [tailwindcss()]
   }
